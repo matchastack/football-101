09d56ffc2d2eb7a09f8d39a4ec0ce46093ad9ed9
Zhan Yi <liauzhanyi@gmail.com>
2024-01-01 17:39:34 +0800

    Add league standings functions

--- backend/api_data.py
+++ backend/api_data.py
@@ -8,16 +8,27 @@ load_dotenv()
 HEADER = {
         "x-rapidapi-host" : os.getenv("RAPIDAPI_HOST"),
         "x-rapidapi-key" : os.getenv("RAPIDAPI_KEY")
-    }
+}
 
+LEAGUES_ID = {
+    "Premier League" : 39,
+    "La-Liga" : 140
+}
+
+############################
+##### Helper Functions #####
+############################
 def make_url(endpoint):
-    return f"https://api-football-vl.p.rapidapi.com/v3{endpoint}"
+    return f"https://api-football-v1.p.rapidapi.com/v3{endpoint}"
 
-def get_api_response(endpoint, headers=HEADER, method="GET"):
+def get_api_response(endpoint, args:dict={}, headers=HEADER, method:str="GET"):
     url = make_url(endpoint)
-    response = requests.request(method, url, headers=headers)
+    response = requests.request(method, url, headers=headers, params=args)
     return response
 
+########################
+###### Endpoints #######
+########################
 def get_leagues_data():
     df = pd.DataFrame()
 
@@ -28,16 +39,65 @@ def get_leagues_data():
     if r.status_code == 200:
         ## unnest top layer json response, convert to pandas df
         df = pd.json_normalize(r.json()["response"])
+        
         ## unnest seasons column (list of dicts)
         df = df.explode("seasons").reset_index(drop=True)
+        
         ## unnest seasons column (dict)
         df = df.join(pd.json_normalize(df["seasons"])).drop(columns=["seasons"])
+        
         ## select columns
         df = df[["league.id", "league.name", "league.type", "start", "end"]]
+        
         ## convert to datetime
         df["start"] = pd.to_datetime(df["start"])
         df["end"] = pd.to_datetime(df["end"])
+        
         ## rename columns
         df.columns = ["id", "name", "type", "start", "end"]
 
     return df
+
+def get_league_standing(season:int, league_id:int=None, team_id:int=None):
+    df = pd.DataFrame()
+    
+    ## set args
+    args = {"season" : season}
+
+    if league_id != None:
+        args["league"] = league_id
+
+    if team_id != None:
+        args["team"] = team_id
+
+    ## get standings data from api
+    r = get_api_response("/standings", args=args)
+
+    ## if response is successful, convert to pandas df
+    if r.status_code == 200:
+        ## unnest top layer json response, convert to pandas df
+        response_list = pd.json_normalize(r.json()["response"])["league.standings"][0][0]
+        df = pd.DataFrame(response_list)
+
+        ## select columns
+        df = df.drop(columns=["group", "status", "description", "update"])
+
+        ## rename columns
+        df["team"] = df["team"].apply(lambda x: x["name"])
+
+        ## unnest columns
+        def expand_col(col):
+            return pd.json_normalize(col).add_prefix(f"{col.name}.")
+        
+        df = df.join(expand_col(df["all"])).drop(columns=["all"])
+        df = df.join(expand_col(df["home"])).drop(columns=["home"])
+        df = df.join(expand_col(df["away"])).drop(columns=["away"])
+
+    return df
+
+## get standings for specific leagues
+def get_premier_league_standing(season:int):
+    return get_league_standing(season, LEAGUES_ID["Premier League"])
+
+def get_laliga_standing(season:int):
+    return get_league_standing(season, LEAGUES_ID["La-Liga"])
