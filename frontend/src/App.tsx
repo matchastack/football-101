198533f0f5bff466b83fc42a59c16370121f7bf8
Zhan Yi <liauzhanyi@gmail.com>
2024-01-05 20:53:18 +0800

    Add Fixtures page to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Table from "./pages/Table";
+import Fixtures from "./pages/Fixtures";
 
 function App() {
     return (
@@ -9,7 +10,12 @@ function App() {
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="/home" element={<Home />} />
+                    <Route path="/premier-league" element={<Table />} />
                     <Route path="/premier-league/teams" element={<Table />} />
+                    <Route
+                        path="/premier-league/fixtures"
+                        element={<Fixtures />}
+                    />
                     <Route path="*" element={<h1>404 Not Found</h1>} />
                 </Routes>
             </BrowserRouter>
